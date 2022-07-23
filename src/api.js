74d8bb09394013f10ba33a5cb1ce4292b3b7a50f
ajkumarrayoff@gmail.com <ajkumarrayoff@gmail.com>
2022-07-24 00:18:17 +0530

    react weather app deployment

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "238021f959msh13cec7dd12b35eap1a01b2jsnef2b6c09ce14",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "5c6d86624fa5682928bfdbc999ba4bb1";
